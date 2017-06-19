89a7e85729ae03be46642c947ce3fdf6c00d0fc0
unknown <footehud@gmail.com>
2017-06-19 11:48:26 -0400

    setting up clock and testign form values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,11 @@
 
 //global variables 
+//score related variables 
+var correct = 0;
+var incorrect = 0;
+
+//timer related variables 
+var countDown = 2;
 
 
 
@@ -8,19 +14,71 @@
 
 
 
-
-
-
-
-//functions 
-
+//form interactivity related variables? 
 
 
 
 
 
 
+///////////////////functions 
+
+
+//starts the clock once the button is clicked and counts down from 2 mins or 120000 milliseconds for the form completion 
+/*function startClock(minutes, seconds){
+var mins =2;
+var sec = 60;
+document.getElementbyId("timerDisplay").innerHTML = mins + ":" + sec;
+	
+	sec--;
+
+	if(sec == 00){
+
+		mins--;
+		if(mins == 0){
+			alert("time is up!");
+		}
+	}
+
+
+}
+
+*/
+
+function startClock(){
+	var sec = setInterval(startclock(){
+		math.floor(currentTime % (1000 * 60))/1000);
+	})
+
+$("#timerDisplay").html = secs;
+
+	if(currentTime < 0){
+
+		clearInterval(sec);
+		$("timerDisplay").html = "Time is up!";
+	}
+
+}, 1000);
+
+//tallys up the number of correct and incorrect responses (the correct have a value of 1 in the buttons)
+
+function countQuestions(){
 
 
 
-//game flow 
+
+}
+
+
+
+
+//Event flow for the quiz game
+
+$(document).ready(function() {
+	
+	setInterval(startclock, 1000);
+
+	}
+
+
+
