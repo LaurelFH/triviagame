7b213f247a3c381684c3d34a267ab2d4e26c23af
LeeAFH <footehud@gmail.com>
2017-06-19 19:38:54 -0400

    styling and working on timer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,18 @@
-
-//global variables 
 //score related variables 
 var correct = 0;
 var incorrect = 0;
 var totalQuestions = 4;
 
 //timer related variables 
-var countDown = "";
-
-
-//click event for the button
-$("#timerDisplay").click(startClock);
-
-
-
+var countDown = 60;
 
 
 ///////////////////functions 
 
 //starts the clock once the button is clicked and counts down from 2 mins or 120000 milliseconds for the form completion 
-/*function startClock(minutes, seconds){
+
+
+function startClock(minutes, seconds){
 var mins =2;
 var sec = 60;
 document.getElementbyId("timerDisplay").innerHTML = mins + ":" + sec;
@@ -33,21 +26,23 @@ document.getElementbyId("timerDisplay").innerHTML = mins + ":" + sec;
 			alert("time is up!");
 		}
 	}
+}
+
+
+
+//starts the clock when the button is pressed 
+
+/*function startClock(){
+	var countDown = 60;
+	var timer = setInterval(countDown--, 1000);
+	
+		if (countDown == 0){
+
+	}
 
 
 }
-
 */
-
-function startClock(){
-	var timeLeft = 2;
-	var seconds;
-
-
-
-
-}
-
 //displays the values of the clock in the timerdisplay space 
 
 //tallys up the number of correct and incorrect responses (the correct have a value of 1 in the buttons)
@@ -57,7 +52,6 @@ function countQuestions(){
  	checkQuestion("examquestions-0");
  	checkQuestion("examquestions-1");
 
-
  	//need to find incorrect, we have the total and the number correct total -correct?
 
 
@@ -66,22 +60,22 @@ function countQuestions(){
 function checkQuestion(answer){
 
 	if(document.getElementById(answer).checked){
-
  		correct += document.getElementbyId(answer).value;
  	}
-
-
 }
-
-
 
 
 //Event flow for the quiz game
 
 $(document).ready(function() {
-	//start the coutndown clock when clicked 	
-	$("button").click(startClock()
-		alert("here we go");
+	
+	//click event for the button
+	$("button").click(function(){
+		startClock();
+
+
+	});
+
 	});
 
 		//
